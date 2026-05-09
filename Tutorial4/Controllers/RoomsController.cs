bd012ba2b5fbc799859fef7c1b099d359331aa20
imsai-8080 <s32304@pjwstk.edu.pl>
2026-05-09 20:28:05 +0200

    Dodanie metod HttpGet do klasy RoomsController

--- Tutorial4/Controllers/RoomsController.cs
+++ Tutorial4/Controllers/RoomsController.cs
@@ -1,6 +1,45 @@
-﻿namespace Tutorial4.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using Tutorial4.Data;
+using Tutorial4.Models;
 
-public class RoomsController
+namespace Tutorial4.Controllers
 {
-    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RoomsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll([FromQuery] int? minCapacity, [FromQuery] bool? hasProjector, [FromQuery] bool? activeOnly)
+        {
+            var rooms = MockDataContext.Rooms.AsQueryable();
+
+            if (minCapacity.HasValue)
+                rooms = rooms.Where(r => r.Capacity >= minCapacity.Value);
+
+            if (hasProjector.HasValue)
+                rooms = rooms.Where(r => r.HasProjector == hasProjector.Value);
+
+            if (activeOnly.HasValue && activeOnly.Value)
+                rooms = rooms.Where(r => r.IsActive);
+
+            return Ok(rooms.ToList());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var room = MockDataContext.Rooms.FirstOrDefault(r => r.Id == id);
+            if (room == null) return NotFound();
+            return Ok(room);
+        }
+
+        [HttpGet("building/{buildingCode}")]
+        public IActionResult GetByBuilding(string buildingCode)
+        {
+            var rooms = MockDataContext.Rooms.Where(r => r.BuildingCode.Equals(buildingCode, StringComparison.OrdinalIgnoreCase)).ToList();
+            return Ok(rooms);
+        }
+
+        
+    }
 }
