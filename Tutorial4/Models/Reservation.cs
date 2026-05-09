329c90f909b75d0e43cc7937413e3027248119bc
imsai-8080 <s32304@pjwstk.edu.pl>
2026-05-09 19:45:24 +0200

    Uzupełnienie klasy Reservation

--- Tutorial4/Models/Reservation.cs
+++ Tutorial4/Models/Reservation.cs
@@ -1,6 +1,29 @@
-﻿namespace Tutorial4.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class Reservation
+namespace Tutorial4.Models
 {
-    
+    public class Reservation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int RoomId { get; set; }
+
+        [Required(ErrorMessage = "Organizator jest wymagany")]
+        public string OrganizerName { get; set; }
+
+        [Required(ErrorMessage = "Temat jest wymagany")]
+        public string Topic { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public TimeSpan StartTime { get; set; }
+
+        [Required]
+        public TimeSpan EndTime { get; set; }
+
+        public string Status { get; set; } // np. planned, confirmed, cancelled
+    }
 }
