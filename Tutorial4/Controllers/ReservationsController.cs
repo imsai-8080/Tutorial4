71f1ffb63a7caee5ddc2d9d9b766b1bc5e7d6047
imsai-8080 <s32304@pjwstk.edu.pl>
2026-05-09 21:47:28 +0200

    Dodanie do klasy metod HttpPost, HttpPut i HttpDelete do klasy ReservationsController

--- Tutorial4/Controllers/ReservationsController.cs
+++ Tutorial4/Controllers/ReservationsController.cs
@@ -32,5 +32,60 @@ namespace Tutorial4.Controllers
             if (res == null) return NotFound();
             return Ok(res);
         }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Reservation newRes)
+        {
+            if (newRes.EndTime <= newRes.StartTime)
+                return BadRequest("Czas zakończenia musi być późniejszy niż rozpoczęcia.");
+
+            
+            var room = MockDataContext.Rooms.FirstOrDefault(r => r.Id == newRes.RoomId);
+            if (room == null) return BadRequest("Wybrana sala nie istnieje.");
+            
+            if (!room.IsActive)
+                return BadRequest("Nie można rezerwować nieaktywnej sali.");
+            
+            bool hasConflict = MockDataContext.Reservations.Any(r => 
+                r.RoomId == newRes.RoomId && 
+                r.Date.Date == newRes.Date.Date &&
+                r.StartTime < newRes.EndTime && 
+                newRes.StartTime < r.EndTime);
+
+            if (hasConflict)
+                return Conflict("Sala jest już zarezerwowana w tym czasie.");
+
+            newRes.Id = MockDataContext.Reservations.Any() ? MockDataContext.Reservations.Max(r => r.Id) + 1 : 1;
+            MockDataContext.Reservations.Add(newRes);
+            
+            return CreatedAtAction(nameof(GetById), new { id = newRes.Id }, newRes);
+        }
+
+        
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Reservation updatedRes)
+        {
+            var res = MockDataContext.Reservations.FirstOrDefault(r => r.Id == id);
+            if (res == null) return NotFound();
+            
+            res.OrganizerName = updatedRes.OrganizerName;
+            res.Topic = updatedRes.Topic;
+            res.Date = updatedRes.Date;
+            res.StartTime = updatedRes.StartTime;
+            res.EndTime = updatedRes.EndTime;
+            res.Status = updatedRes.Status;
+
+            return Ok(res);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var res = MockDataContext.Reservations.FirstOrDefault(r => r.Id == id);
+            if (res == null) return NotFound();
+
+            MockDataContext.Reservations.Remove(res);
+            return NoContent();
+        }
     }
 }
